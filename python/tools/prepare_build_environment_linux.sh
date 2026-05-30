371beb5d62eee5d3e70b5264b48a605eee1a4f75
Lysander Sokolov <Kijinmaru@proton.me>
2026-05-30 20:59:01 +0000

    Fix prepare script more

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -21,7 +21,7 @@ if [ "$CIBW_ARCHS" == "aarch64" ]; then
 else
     dnf install -y dnf-plugins-core
     # Install CUDA:
-    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
     # error mirrorlist.centos.org doesn't exists anymore.
     sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
     sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
@@ -56,23 +56,36 @@ else
     rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
     dnf install -y intel-oneapi-mkl-devel-$ONEAPI_VERSION
 
-    ONEDNN_VERSION=3.1.1
-    curl -L -O https://github.com/oneapi-src/oneDNN/archive/refs/tags/v${ONEDNN_VERSION}.tar.gz
-    tar xf *.tar.gz && rm *.tar.gz
-    cd oneDNN-*
-    cmake -DCMAKE_BUILD_TYPE=Release -DONEDNN_LIBRARY_TYPE=STATIC -DONEDNN_BUILD_EXAMPLES=OFF -DONEDNN_BUILD_TESTS=OFF -DONEDNN_ENABLE_WORKLOAD=INFERENCE -DONEDNN_ENABLE_PRIMITIVE="CONVOLUTION;REORDER" -DONEDNN_BUILD_GRAPH=OFF .
-    make -j$(nproc) install
-    cd ..
-    rm -r oneDNN-*
+    (
+      ONEDNN_VERSION=3.1.1
+      ONEDNN_OWNER="Phobos1641"
+      ONEDNN_REPO="oneDNN"
+      ONEDNN_TAG="v${ONEDNN_VERSION}-phobos"
+      ONEDNN_PREFIX="/usr"
 
-    OPENMPI_VERSION=4.1.6
-    curl -L -O https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-${OPENMPI_VERSION}.tar.bz2
-    tar xf *.tar.bz2 && rm *.tar.bz2
-    cd openmpi-*
-    ./configure
-    make -j$(nproc) install
-    cd ..
-    rm -r openmpi-*
+      ONEDNN_ASSET="onednn-${ONEDNN_TAG}-linux-x86_64-glibc2.34.tar.gz"
+      ONEDNN_URL="https://github.com/${ONEDNN_OWNER}/${ONEDNN_REPO}/releases/download/${ONEDNN_TAG}"
+
+      ONEDNN_TMP="$(mktemp -d)"
+      curl -fsSL -o "${ONEDNN_TMP}/${ONEDNN_ASSET}"        "${ONEDNN_URL}/${ONEDNN_ASSET}"
+      curl -fsSL -o "${ONEDNN_TMP}/${ONEDNN_ASSET}.sha256" "${ONEDNN_URL}/${ONEDNN_ASSET}.sha256"
+      ( cd "$ONEDNN_TMP" && sha256sum -c "${ONEDNN_ASSET}.sha256" )
+
+      mkdir -p "$ONEDNN_PREFIX"
+      tar -xzf "${ONEDNN_TMP}/${ONEDNN_ASSET}" -C "$ONEDNN_PREFIX"
+      rm -rf "$ONEDNN_TMP"
+    )
+
+    (
+      OPENMPI_VERSION=4.1.6
+      curl -L -O https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-${OPENMPI_VERSION}.tar.bz2
+      tar xf *.tar.bz2 && rm *.tar.bz2
+      cd openmpi-*
+      ./configure
+      make -j$(nproc) install
+      cd ..
+      rm -r openmpi-*
+    )
     export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
 fi
 
@@ -81,7 +94,19 @@ mkdir build-release && cd build-release
 if [ "$CIBW_ARCHS" == "aarch64" ]; then
     cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CLI=OFF -DWITH_MKL=OFF -DOPENMP_RUNTIME=COMP -DCMAKE_PREFIX_PATH="/opt/OpenBLAS" -DWITH_OPENBLAS=ON -DWITH_RUY=ON ..
 else
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-msse4.1" -DBUILD_CLI=OFF -DWITH_DNNL=ON -DOPENMP_RUNTIME=COMP -DWITH_CUDA=ON -DWITH_CUDNN=OFF -DCUDA_DYNAMIC_LOADING=ON -DCUDA_NVCC_FLAGS="-Xfatbin=-compress-all" -DCUDA_ARCH_LIST="Common"  -DWITH_TENSOR_PARALLEL=ON ..
+    cmake \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_FLAGS="-msse4.1" \
+      -DBUILD_CLI=OFF \
+      -DWITH_DNNL=ON \
+      -DOPENMP_RUNTIME=COMP \
+      -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_TOOLKIT_ROOT_DIR} \
+      -DWITH_CUDA=ON \
+      -DWITH_CUDNN=ON \
+      -DCUDA_DYNAMIC_LOADING=ON \
+      -DCUDA_NVCC_FLAGS="-Xfatbin=-compress-all" \
+      -DWITH_TENSOR_PARALLEL=ON \
+      ..
 fi
 
 VERBOSE=1 make -j$(nproc) install
