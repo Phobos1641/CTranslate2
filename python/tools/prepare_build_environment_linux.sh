dff072643ce10ab30254880eed2052c91ae980a5
Lysander Sokolov <Kijinmaru@proton.me>
2026-05-30 14:24:22 +0000

    CI: install CUDA 13.2

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -20,21 +20,21 @@ if [ "$CIBW_ARCHS" == "aarch64" ]; then
 
 else
     dnf install -y dnf-plugins-core
-    # Install CUDA 12.8:
+    # Install CUDA 13.2:
     dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
     # error mirrorlist.centos.org doesn't exists anymore.
     sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
     sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
     sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
     dnf install --setopt=obsoletes=0 -y \
-        cuda-nvcc-12-8-12.8.93-1 \
-        cuda-cudart-devel-12-8-12.8.90-1 \
-        libcurand-devel-12-8-10.3.9.90-1 \
-        libcudnn9-devel-cuda-12-9.10.2.21-1 \
-        libcublas-devel-12-8-12.8.4.1-1 \
-        libnccl-2.26.2-1+cuda12.8 \
-        libnccl-devel-2.26.2-1+cuda12.8
-    ln -s cuda-12.8 /usr/local/cuda
+        cuda-nvcc-13-2 \
+        cuda-cudart-devel-13-2 \
+        libcurand-devel-13-2 \
+        libcudnn9-devel-cuda-13 \
+        libcublas-devel-13-2 \
+        libnccl-2.30.4-1+cuda13.2 \
+        libnccl-devel-2.30.4-1+cuda13.2
+    ln -s cuda-13.2 /usr/local/cuda
 
     ONEAPI_VERSION=2025.3.0
     dnf config-manager --add-repo https://yum.repos.intel.com/oneapi
